73b09fa7cd43a893de3e494386b974174312efb4
Elijah Kennedy <elijahclimbs@gmail.com>
2024-10-21 20:31:33 -0400

    triv

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars prefer-const @typescript-eslint/no-unused-vars */
 'use client'
 
 // Here is a claude explanation on how to split layers into diff post-processing treatments:
